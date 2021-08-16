3241088ba1b9344e1d543a42195aa33bce5877e7
Felipe Lima <flf.felipe@gmail.com>
2021-08-15 22:28:06 -0300

    first entries

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # credit-check-service
 Mocks a service to request credit approvement.
+
+## Environment Variables
+
+```bash
+KAFKA_URL=localhost:29092
+```
---
+++ src/kafka.js
@@ -0,0 +1,22 @@
+const kafka = require('kafka-node');
+const { PENDING_TOPIC } = require('./constants');
+
+require('dotenv').config();
+
+// kafka client
+const client = new kafka.KafkaClient({ kafkaHost: process.env.KAFKA_URL });
+
+const topicsToCreate = [
+  {
+    topic: PENDING_TOPIC,
+    partitions: 1,
+    replicationFactor: 1,
+  },
+];
+
+client.createTopics(topicsToCreate, (error, result) => {
+  console.error(error);
+  console.log(result);
+});
+
+exports.producer = new kafka.Producer(client);
